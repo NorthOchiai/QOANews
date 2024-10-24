e99aea0aed5dd19d2dd5de31d45efb5024dd3146
北落合 <95562214+NorthOchiai@users.noreply.github.com>
2024-10-24 09:58:29 +0900

    Update NHKNewsGet.py

--- NHKNewsGet.py
+++ NHKNewsGet.py
@@ -1,32 +1,32 @@
 import json
 import feedparser
-import pprint
 import os
 
-urlBase="https://www.nhk.or.jp/rss/news/cat"
+urlBase = "https://www.nhk.or.jp/rss/news/cat"
 
-#取得してまずdictにする
-newsList=[]
+# 取得してまずdictにする
+newsDict = []
 for i in range(8):
     url = urlBase + str(i) + ".xml"
     raw = feedparser.parse(url)
-    
-    #ジャンルごとのニュース
-    news=[]
-    news['published']=raw['feed']['updated']
+
+    # ジャンルごとのニュース
+    newsList = {}
+    newsList['updated'] = raw['feed']['updated']
+    news = []
     for rawArticle in raw['entries']:
-        article={}
+        article = {}
         article['title'] = rawArticle['title']
         article['summary'] = rawArticle['summary']
         article['published'] = rawArticle['published']
         article['link'] = rawArticle['link']
         news.append(article)
-    newsList.append(news)
-    
-#jsonに変換
-newsJson=json.dumps(newsList,ensure_ascii=False)
+    newsList['news'] = news
 
-#ファイル出力
+# jsonに変換
+newsJson = json.dumps(newsList, ensure_ascii=False)
+
+# ファイル出力
 sourcePath = os.path.dirname(__file__)
 f = open(sourcePath+"/news", "w", encoding='utf-8')
 f.write(newsJson)

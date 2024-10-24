85e2f4e7239f88f5f5ba351677dc612da80fe61c
北落合 <95562214+NorthOchiai@users.noreply.github.com>
2024-10-24 09:23:42 +0900

    Update NHKNewsGet.py

--- NHKNewsGet.py
+++ NHKNewsGet.py
@@ -1,5 +1,6 @@
 import json
 import feedparser
+import pprint
 import os
 
 urlBase="https://www.nhk.or.jp/rss/news/cat"
@@ -10,11 +11,14 @@ for i in range(8):
     url = urlBase + str(i) + ".xml"
     raw = feedparser.parse(url)
     
+    #ジャンルごとのニュース
     news=[]
+    news.append(raw['feed']['updated'])
     for rawArticle in raw['entries']:
         article={}
         article['title'] = rawArticle['title']
         article['summary'] = rawArticle['summary']
+        article['published'] = rawArticle['published']
         article['link'] = rawArticle['link']
         news.append(article)
     newsList.append(news)
@@ -24,6 +28,6 @@ newsJson=json.dumps(newsList,ensure_ascii=False)
 
 #ファイル出力
 sourcePath = os.path.dirname(__file__)
-f = open(sourcePath+"/NewsData.json", "w", encoding='utf-8')
+f = open(sourcePath+"/news", "w", encoding='utf-8')
 f.write(newsJson)
 f.close()

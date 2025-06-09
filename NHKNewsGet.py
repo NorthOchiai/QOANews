e3e6de820f0fa411f2a8d0f153152f4bba52fd44
北落合 <95562214+Qitaochi@users.noreply.github.com>
2025-06-09 13:51:05 +0900

    Update NHKNewsGet.py

--- NHKNewsGet.py
+++ NHKNewsGet.py
@@ -3,17 +3,42 @@ import datetime
 import feedparser
 import os
 
-urlBase = "https://www.nhk.or.jp/rss/news/cat"
+urlBase1 = "https://www.nhk.or.jp/rss/news/cat"
+urlBase2 = "https://www3.nhk.or.jp/lnews/"
 
 # 取得してまずdictにする
 newsDict = []
 for i in range(8):
-    url = urlBase + str(i) + ".xml"
+    url = urlBase1 + str(i) + ".xml"
     raw = feedparser.parse(url)
 
     # ジャンルごとのニュース
     newsList = {}
-    newsList['cat']=raw['feed']['title']
+    newsList['cat'] = raw['feed']['title']
+    newsList['updated'] = raw['feed']['updated']
+    news = []
+    for rawArticle in raw['entries']:
+        article = {}
+        article['title'] = rawArticle['title']
+        article['summary'] = rawArticle['summary']
+        article['published'] = rawArticle['published']
+        article['link'] = rawArticle['link']
+        news.append(article)
+    newsList['news'] = news
+    newsDict.append(newsList)
+
+#地域ごとのニュースも取れるみたいなのでいれておく
+#県庁所在地
+kencho = {"sapporo","aomori","morioka","sendai","akita","yamagata","fukushima","mito","utsunomiya","maebashi","saitama","chiba","tokyo","yokohama","niigata","toyama","kanazawa","fukui","kofu","nagano","gifu","shizuoka","nagoya","tsu","otsu","kyoto","osaka","kobe","nara","wakayama","tottori","matsue","okayama","hiroshima","yamaguchi","tokushima","takamatsu","matsuyama","kochi","fukuoka","saga","nagasaki","kumamoto","oita","miyazaki","kagoshima","naha"}
+    
+#47都道府県分
+for ken in kencho:
+    url = urlBase2 + ken + "/toplist.xml"
+    raw = feedparser.parse(url)
+
+    #県ごとのニュース
+    newsList = {}
+    newsList['cat'] = ken
     newsList['updated'] = raw['feed']['updated']
     news = []
     for rawArticle in raw['entries']:

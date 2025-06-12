c48881f906b0446b190fb649be9b34f4e9d61b1e
北落合 <95562214+Qitaochi@users.noreply.github.com>
2025-06-12 13:44:49 +0900

    Update NHKNewsGet.py

--- NHKNewsGet.py
+++ NHKNewsGet.py
@@ -24,24 +24,24 @@ for i in range(8):
         article['published'] = rawArticle['published']
         article['link'] = rawArticle['link']
         news.append(article)
-    newsList['news'] = news
+    newsList['newsList'] = news
     newsDict.append(newsList)
 
-#地域ごとのニュースも取れるみたいなのでいれておく
-#県庁所在地(東京はshutoken, 那覇はokinawaっぽい)
-kencho = ["sapporo","aomori","morioka","sendai","akita","yamagata","fukushima",
-          "mito","utsunomiya","maebashi","saitama","chiba","shutoken","yokohama",
-          "niigata","toyama","kanazawa","fukui","kofu","nagano","gifu","shizuoka","nagoya",
-          "tsu","otsu","kyoto","osaka","kobe","nara","wakayama",
-          "tottori","matsue","okayama","hiroshima","yamaguchi","tokushima","takamatsu","matsuyama","kochi",
-          "fukuoka","saga","nagasaki","kumamoto","oita","miyazaki","kagoshima","okinawa"]
+# 地域ごとのニュースも取れるみたいなのでいれておく
+# 県庁所在地(東京はshutoken, 那覇はokinawaっぽい)
+kencho = ["sapporo", "aomori", "morioka", "sendai", "akita", "yamagata", "fukushima",
+          "mito", "utsunomiya", "maebashi", "saitama", "chiba", "shutoken", "yokohama",
+          "niigata", "toyama", "kanazawa", "fukui", "kofu", "nagano", "gifu", "shizuoka", "nagoya",
+          "tsu", "otsu", "kyoto", "osaka", "kobe", "nara", "wakayama",
+          "tottori", "matsue", "okayama", "hiroshima", "yamaguchi", "tokushima", "takamatsu", "matsuyama", "kochi",
+          "fukuoka", "saga", "nagasaki", "kumamoto", "oita", "miyazaki", "kagoshima", "okinawa"]
 
-#47都道府県分
+# 47都道府県分
 for ken in kencho:
     url = urlBase2 + ken + "/toplist.xml"
     raw = feedparser.parse(url)
 
-    #県ごとのニュース
+    # 県ごとのニュース
     newsList = {}
     newsList['cat'] = ken
     newsList['updated'] = raw['feed']['updated']
@@ -53,7 +53,7 @@ for ken in kencho:
         article['published'] = rawArticle['published']
         article['link'] = rawArticle['link']
         news.append(article)
-    newsList['news'] = news
+    newsList['newsList'] = news
     newsDict.append(newsList)
 
 # jsonに変換
@@ -64,10 +64,10 @@ sourcePath = os.path.dirname(__file__)
 f = open(sourcePath+"/news.json", "w", encoding='utf-8')
 f.write(newsJson)
 f.close()
-f = open(sourcePath+"/date.json", "w",encoding='utf-8')
+f = open(sourcePath+"/date.json", "w", encoding='utf-8')
 
-dateDict={}
-dateDict['committed']=str(datetime.datetime.now())
-dateDictJson = json.dumps(dateDict,ensure_ascii=False)
+dateDict = {}
+dateDict['committed'] = str(datetime.datetime.now())
+dateDictJson = json.dumps(dateDict, ensure_ascii=False)
 f.write(dateDictJson)
 f.close()

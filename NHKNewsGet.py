15cd655a140a5c264fabc9dccacae173ff9c7be0
北落合 <95562214+Qitaochi@users.noreply.github.com>
2025-06-15 20:23:46 +0900

    Update NHKNewsGet.py

--- NHKNewsGet.py
+++ NHKNewsGet.py
@@ -11,7 +11,6 @@ newsDict = []
 for i in range(8):
     url = urlBase1 + str(i) + ".xml"
     raw = feedparser.parse(url)
-
     # ジャンルごとのニュース
     newsList = {}
     newsList['cat'] = raw['feed']['title']
@@ -36,14 +35,20 @@ kencho = ["sapporo", "aomori", "morioka", "sendai", "akita", "yamagata", "fukush
           "tottori", "matsue", "okayama", "hiroshima", "yamaguchi", "tokushima", "takamatsu", "matsuyama", "kochi",
           "fukuoka", "saga", "nagasaki", "kumamoto", "oita", "miyazaki", "kagoshima", "okinawa"]
 
+todofuken = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+             "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
+             "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県",
+             "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+             "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県",
+             "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+
 # 47都道府県分
 for ken in kencho:
     url = urlBase2 + ken + "/toplist.xml"
     raw = feedparser.parse(url)
-
     # 県ごとのニュース
     newsList = {}
-    newsList['cat'] = ken
+    newsList['cat'] = todofuken
     newsList['updated'] = raw['feed']['updated']
     news = []
     for rawArticle in raw['entries']:
